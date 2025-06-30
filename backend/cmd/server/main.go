581f2794fb26274198dc0f3373a95b2763b9240d
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-07-01 06:36:24 +0900

    main.go修正

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/k-kanke/order-system/config"
+	"github.com/k-kanke/order-system/router"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	config.LoadEnv()
-
+	r := router.SetupRouter()
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
