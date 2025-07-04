71e6aade3666de56586b490bca5daabfed43b17c
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-07-04 13:43:43 +0900

    GetMenuコントローラ作成

---
+++ backend/controller/menu.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/k-kanke/order-system/model"
+	"github.com/k-kanke/order-system/service"
+)
+
+func GetMenu(c *gin.Context) {
+	token, err := service.GetAccessTokenWithCache()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get token"})
+		return
+	}
+
+	levels := []string{"1", "2", "3"}
+	var allCategories []model.Category
+	for _, level := range levels {
+		categories, err := service.FetchCategories(token, level)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		allCategories = append(allCategories, categories...)
+	}
+
+	products, err := service.FetchProducts(token)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"categories": allCategories,
+		"products":   products,
+	})
+}
